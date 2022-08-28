6ce41322089566f5d14910fa789094fdf657a763
Sasha <shbam@bk.ru>
2022-08-29 01:56:09 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/Main.java
@@ -0,0 +1,10 @@
+package com.geekbrains;
+/*
+    ! Имя класса должно обязательно начинаться с ЗАГЛАВНОЙ буквы !
+    ! Имя класса в горбатом стиле !
+ */
+public class Main {
+    public static void main(String[] args){
+        // write your code here
+    }
+}
