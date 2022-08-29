0f8007733145b4f82826f4bfd0bfcd789e1a1db8
Sasha <shbam@bk.ru>
2022-08-30 00:04:27 +0300

    Практическое задание 2 выполнено в пакете lesson2

---
+++ src/main/java/com/geekbrains/lesson2/BasicConstructions.java
@@ -0,0 +1,95 @@
+package com.geekbrains.lesson2;
+
+public class BasicConstructions {
+    public static void main(String[] args) {
+        System.out.println( "1. метод");
+        System.out.println( method1( 5, 16 ) );
+
+        System.out.println("2. метод");
+        method2(1);
+
+        System.out.println("3. метод");
+        System.out.println( method3( 1 ) );
+
+        System.out.println("4. метод");
+        method4( "проба", 3);
+
+        System.out.println("5. метод - определяем високосный или нет");
+        System.out.println( method5( 800 ));
+    }
+
+    /**  5-ый метод, который определяет,
+     *   является ли год високосным,
+     *   и возвращает boolean (високосный - true, не високосный - false).
+     *   Каждый 4-й год является високосным,    (делимость на 4,    прим. СШБ)
+     *   кроме каждого 100-го,                  (делимость на 100,  прим. СШБ)
+     *   при этом каждый 400-й – високосный     (делимость на 400,  прим. СШБ).
+     */
+    public static boolean method5(int year) {
+        //System.out.print( year + " - ");
+        if( year%4 == 0 ){                  // выделили все возможные високосные
+            if ( year%100 != 0) {
+                return true;                // все Високосные кроме 100-ых
+            } else if ( year%400 == 0) {
+                return true;                // из 100-ых разрешили только 400-ые Високосные
+            }else {
+                return false;               // все НЕвисокосные 100-ые, но уже без 400-х
+            }
+        }else {
+            return false;                   // все НЕвисокосные и не 100-ые
+        }
+    }
+
+    /**  4-ый метод, которому в качестве аргументов передается строка и число,
+     *   метод должен отпечатать в консоль указанную строку,
+     *   указанное количество раз;
+     */
+    public static void method4( String str, int i ) {
+        for( int j=i; j > 0; j--){
+            //System.out.print( j + ". ");
+            System.out.println(str);
+        }
+    }
+
+    /**  3-ий метод, которому в качестве параметра передается целое число.
+     *   Метод должен вернуть true,
+     *   если число отрицательное,
+     *   и вернуть false если положительное.
+     */
+    public static boolean method3( int i) {
+        if( i < 0 ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**  2-ой метод, которому в качестве параметра передается целое число,
+     *   метод должен напечатать в консоль,
+     *   положительное ли число передали или отрицательное.
+     *   Замечание: ноль считаем положительным числом.
+     */
+    public static void method2( int i) {
+        //System.out.print( i + " - ");
+        if (i>=0){
+            System.out.println("положительное");
+        }else {
+            System.out.println("отрицательное");
+        }
+    }
+
+    /**  1-ый метод, принимающий на вход два целых числа и проверяющий,
+     *  что их сумма лежит в пределах от 10 до 20 (включительно),
+     *  если да – вернуть true,
+     *  в противном случае – false.
+     */
+    public static boolean method1(int int1, int int2 ) {
+        int sum = int1 + int2 ;
+        //System.out.println( "sum: " + int1 + " + " + int2 + " = " + sum );
+        if ( (10 <= sum) && (sum <= 20) ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
